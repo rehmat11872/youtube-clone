5d1c854a0b3da51657e74ca91c96bc06e35ce452
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2022-09-07 11:53:05 +0000

    Sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { height } from '@mui/system';
+import Sidebar from './Sidebar';
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: {sx: "column", md: "row"} }}>
+      <Box sx={{ height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #d3d3d3', px: {sx: 0, md:2,}  }}>
+
+      <Sidebar />
+
+      <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff'}}>
+       CopyRight 2022 JSM Media
+      </Typography>
+      </Box>
+    </Stack>
   )
 }
 
